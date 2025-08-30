9caaee0368a9afdb6a0e41bca384fea48b11bcb3
Alexander Balagurov <entrue19@gmail.com>
2025-08-30 20:11:55 +0300

    add xcode project

---
+++ SimilarPhotosApp/SimilarPhotosApp/SimilarPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarPhotosAppApp.swift
+//  SimilarPhotosApp
+//
+//  Created by Oleksandr Balahurov on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimilarPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
